6f2f743f0cef3b4cd48da2b640b38c5072fb9ac8
SGlogiewic <sglogiewic@my.gcu.edu>
2023-02-12 23:31:24 -0700

    First code drop
    Project currently in a bugged state and will not run.

---
+++ AloeHealth/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module AloeHealth {
+	exports com.example.demo;
+
+	requires spring.beans;
+	requires spring.boot;
+	requires spring.boot.autoconfigure;
+	requires spring.context;
+	requires spring.core;
+	requires spring.web;
+}
